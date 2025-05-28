333161c5bf11b4edcd76e4fe28e039ada466b834
Geniussino <eugen.emacu@gmail.com>
2025-05-28 21:17:17 +0300

    refactor

---
+++ src/test/java/homework/BaseTest.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class BaseTest {
+    //de facut un exemplu cu un scenariu de test folosind POM
+    //BaseTest+ clasa de test
+    //BasePage+ clasa de page
+    //LoginTest
+}
