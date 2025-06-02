a94e0d08af58d080647d8b9eda96a08236b26f52
Geniussino <eugen.emacu@gmail.com>
2025-06-02 18:24:45 +0300

    refactor

--- src/test/java/homework/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package homework;
-
-public class BaseTest {
-    //de facut un exemplu cu un scenariu de test folosind POM
-    //BaseTest+ clasa de test
-    //BasePage+ clasa de page
-    //LoginTest
-}
---
+++ src/test/java/homework/tests/BaseTest.java
@@ -0,0 +1,13 @@
+package homework.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    //de facut un exemplu cu un scenariu de test folosind POM
+    //BaseTest+ clasa de test
+    //BasePage+ clasa de page
+    //LoginTest
+}
